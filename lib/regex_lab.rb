d84712a8f24f7995f296a3f5e341a77ce3e817e9
Otto Cedeno <otto@ottostacos.com>
2019-10-31 17:15:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,11 +12,15 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text[0] == text.scan(/[A-Z]/)[0] && (text[text.length - 1] == text[text.length - 1].scan(/[.!?]/)[0])
+  #answer from the Flatiron tech coach:
+  text.match(/^[A-Z].+[\.!?]/) ? true : false
+
+  #my first attempt:
+  # text[0] == text.scan(/[A-Z]/)[0] && (text[text.length - 1] == text[text.length - 1].scan(/[.!?]/)[0])
 end
 
 def valid_phone_number?(phone)
   phone.scan(/\d/).length == 10
 end
 
-print valid_phone_number?("234 43 9978")
+print starts_with_a_vowel?("Asshole")
