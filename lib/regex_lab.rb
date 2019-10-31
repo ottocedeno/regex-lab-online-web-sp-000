d0c1db097ced28ba10be6ff44f9155cb89a44e9c
Otto Cedeno <otto@ottostacos.com>
2019-10-31 17:18:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,5 @@
 def starts_with_a_vowel?(word)
-  # word[0] == word.scan(/[aeiou]/i)[0]
-  word.match(/[aeiou]/i) ? true : false
+  word[0] == word.scan(/[aeiou]/i)[0]
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
